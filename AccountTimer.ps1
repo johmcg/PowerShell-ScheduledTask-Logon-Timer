e4943a48fd42f978f9712f5777b92243fe4be99c
johmcg <jpmcg33@gmail.com>
2025-12-07 19:33:44 -0500

    Initial Script

---
+++ AccountTimer.ps1
@@ -0,0 +1,16 @@
+# Define the Wscript.Shell object once at the start
+$wshell = New-Object -ComObject Wscript.Shell
+
+# 5 Minute Warning
+Start-Sleep -Seconds 300
+$wshell.Popup("5 Minutes Remaining", 0, "Timer", 0x1)
+
+# 1 Minute Warning
+Start-Sleep -Seconds 240
+$wshell.Popup("1 Minute Remaining", 0, "Timer", 0x1)
+
+# Time Up
+Start-Sleep -Seconds 60
+$wshell.Popup("Time's Up!", 0, "Timer", 0x1)
+
+Exit
