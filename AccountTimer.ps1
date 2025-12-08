64e4af397f74f86686f45acd82422d0c5184b308
johmcg <jpmcg33@gmail.com>
2025-12-07 20:05:53 -0500

    Added login time log.

--- AccountTimer.ps1
+++ AccountTimer.ps1
@@ -1,12 +1,17 @@
+#Save login time to text file.
+$yourUser = 'Username Here'
+$userInfo = net user $yourUser | Select-String 'Last logon'
+$userInfo >> C:\Users\$yourUser\log.txt # or wherever you like
+
 # Define the Wscript.Shell object once at the start
 $wshell = New-Object -ComObject Wscript.Shell
 
 # 5 Minute Warning
-Start-Sleep -Seconds 300
+Start-Sleep -Seconds 1500
 $wshell.Popup("5 Minutes Remaining", 0, "Timer", 0x1)
 
 # 1 Minute Warning
-Start-Sleep -Seconds 240
+Start-Sleep -Seconds 240 
 $wshell.Popup("1 Minute Remaining", 0, "Timer", 0x1)
 
 # Time Up
